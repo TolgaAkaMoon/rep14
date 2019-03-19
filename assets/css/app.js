902ae2233b493d68f94de03ef940d2b1eccdbcb7
Benjamin Hansen <benjamin.hansen44@gmail.com>
2019-03-19 08:38:53 +0100

    Update filer

--- assets/css/app.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    var backbutton  = document.querySelector('.backbutton');
-    backbutton.addEventListener('click', function(){
-        history.back();
-    });
-});
